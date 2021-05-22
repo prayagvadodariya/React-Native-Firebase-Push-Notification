e9349bae7e4d6f726041a355052e918d51c9747c
unknown <prayagvadodariya@gmail.com>
2021-05-22 11:22:11 +0530

    add notification code

---
+++ App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import {Text, View, Button, StyleSheet} from 'react-native';
+// import { LocalNotification, ScheduledLocalNotification, } from './src/services/LocalPushController';
+
+import RemotePushController from './src/services/RemotePushController';
+
+const App = () => {
+  // const handleButtonPress = () => {
+  //   LocalNotification();
+  // };
+
+  // const handleScheduleNotification = () => {
+  //   ScheduledLocalNotification();
+  // };
+
+  return (
+    <View style={styles.container}>
+      <Text>Press a button to trigger the notification</Text>
+      <RemotePushController />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonContainer: {
+    marginTop: 20,
+  },
+});
+
+export default App;
